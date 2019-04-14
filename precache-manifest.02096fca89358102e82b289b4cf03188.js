1573c3b3f9ec969d7ec669ed336d033a16429c32
Nathan Acosta <git@nathanacosta.com>
2019-04-13 18:37:29 -0600

    Updates

---
+++ precache-manifest.02096fca89358102e82b289b4cf03188.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "76efcf9d39fc107eaff6",
+    "url": "/pomodoro-timer/static/js/runtime~main.76efcf9d.js"
+  },
+  {
+    "revision": "955b8f243d4d6d614259",
+    "url": "/pomodoro-timer/static/js/main.955b8f24.chunk.js"
+  },
+  {
+    "revision": "8d7ebb705c1628a2daa4",
+    "url": "/pomodoro-timer/static/js/2.8d7ebb70.chunk.js"
+  },
+  {
+    "revision": "955b8f243d4d6d614259",
+    "url": "/pomodoro-timer/static/css/main.23e6fe88.chunk.css"
+  },
+  {
+    "revision": "9b1fe55954221734df45e373a43a304a",
+    "url": "/pomodoro-timer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pomodoro-timer/precache-manifest.871d82406b37c53dcbae3d94058be52f.js"
+  "/pomodoro-timer/precache-manifest.02096fca89358102e82b289b4cf03188.js"
 );
 
 workbox.clientsClaim();
